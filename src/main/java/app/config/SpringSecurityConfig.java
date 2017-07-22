5d0eaba2f10bc09f324112e9ee3d9c2083800c71
Cagatay <Cagatay@DESKTOP-IGKU585>
2017-07-22 17:19:19 +0300

    Security

---
+++ src/main/java/app/config/SpringSecurityConfig.java
@@ -0,0 +1,50 @@
+package app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+
+	@Autowired
+	private AccessDeniedHandler accessDeniedHandler;
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/index", "/");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		// rest için
+		// http.csrf().disable().authorizeRequests()
+		// .anyRequest().authenticated()
+		// .and().httpBasic()
+		// .authenticationEntryPoint(authEntryPoint);
+
+		http.csrf().disable().authorizeRequests().antMatchers("/admin").permitAll().antMatchers("/admin/**")
+				.hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+				.and().logout().permitAll().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("cagatay").password("cagatay").roles("USER");
+		auth.inMemoryAuthentication().withUser("cagatay").password("cagatay1").roles("ADMIN");
+
+	}
+
+}
--- src/main/java/app/controller/IndexController.java
+++ src/main/java/app/controller/IndexController.java
@@ -2,15 +2,31 @@ package app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping(value = { "/index" }, method = RequestMethod.GET)
-	public String index(Model model){
+	 @GetMapping({"/","/index"})
+	public String index(){
 		
 		return "index";
 	}
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "/admin";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "/error/403";
+    }
 }
